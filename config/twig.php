7b7051a2101001bc6d138f9245980a95efe054e1
Kévin Albespy <kevin.albespy@gmail.com>
2022-10-22 18:08:05 +0200

    Twiggy babe

--- config/twig.php
+++ config/twig.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// tu indiques ici à Twig où aller chercher tes fichiers de vues
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
